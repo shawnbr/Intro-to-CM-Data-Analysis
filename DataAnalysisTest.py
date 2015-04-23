f0daacd23345586ff5f3e079770b6e0d979ef7e9
shawnbr <shawn.bramson@gmail.com>
2015-04-22 23:32:42 -0400

    frequency of checkins vs popularity

---
+++ DataAnalysisTest.py
@@ -0,0 +1,69 @@
+import os
+import matplotlib.pyplot as plt
+DIR_PATH = os.getcwd() #Get currect directory
+N = 1000
+#f = open(DIR_PATH + os.sep + "data\Gowalla_edges.txt")
+
+popCheckIn = {}
+checkins = {}
+
+def plot(X,Y, title, xlabel, ylabel):
+    #plt.figure()
+    plt.plot(X, Y, 'ro')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+    #plt.savefig('CMC '+name+'.png')
+
+with open(DIR_PATH + os.sep + "data\Gowalla_edges.txt", 'r') as f:
+    #i = 0
+    for line in f:
+        #if i> N:
+        #    break
+        try:
+            popCheckIn[int(line.strip().split()[0])][0] += 1
+        except:
+            popCheckIn[int(line.strip().split()[0])] = [1,0]               
+        #i += 1
+
+
+
+with open(DIR_PATH + os.sep + "data\Gowalla_totalCheckins.txt", 'r') as f:
+    #i = 0
+    for line in f:
+        #if i > N:
+        #    break
+        try:
+            popCheckIn[int(line.strip().split()[0])][1] += 1
+        except:
+            popCheckIn[int(line.strip().split()[0])].append(1)
+        
+        #i += 1
+
+maxVal = -1
+for value in popCheckIn.values():
+    if value[0] > maxVal:
+        maxVal = value[0]
+
+X = []
+Y = []
+temp = {}
+for key in popCheckIn.keys():
+    if popCheckIn[key][0]/float(maxVal) < 0.05:
+        X.append(popCheckIn[key][0]/float(maxVal))
+        Y.append(popCheckIn[key][1])
+        try:
+            temp[popCheckIn[key][0]].append(popCheckIn[key][1])
+        except:
+            temp[popCheckIn[key][0]] = [popCheckIn[key][1]]
+
+#Average 
+Xavg = []
+Yavg = []
+for key in temp.keys():
+    X.append(key)
+    Y.append(sum(temp[key])/float(len(temp[key])))
+plot(X,Y,"Checkins vs Popularity", "Popularity", "Checkins")
+
+    
